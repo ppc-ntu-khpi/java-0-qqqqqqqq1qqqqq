73cf19e5f3039afcef7d817a822825c2dcdef4e3
qqqqqqqq1qqqqq <92012146+qqqqqqqq1qqqqq@users.noreply.github.com>
2022-01-31 09:08:10 +0200

    Create Quotation.java

---
+++ src/Quotation.java
@@ -0,0 +1,7 @@
+public class Quotation {
+  public String quote = "волк не тот волк кто волк а тот кто волк";
+  public String author = "я";
+  public void display() {
+    System.out.println(quote + "\nAuthor: " + author);
+  }
+}
